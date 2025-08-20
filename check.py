b28f3b47b6b8fcc1e691caaf6889c19394875420
Saad Amir <saadd.aamirr@gmail.com>
2025-08-20 10:46:06 +0500

    added saad15 folder that contains a sample .aaf file, you can edit the json file to include your video/videos or audio/audios, also added sample video and audio

--- check.py
+++
@@ -1,4 +0,0 @@
-import aaf2
-
-with aaf2.open('Timeline 1.aaf', 'r') as f:
-    print(f.content)  # or explore what the file contains
--- soundenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/saadamir/Documents/sound/soundenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
